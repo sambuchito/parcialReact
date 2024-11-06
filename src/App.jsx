09160a42c118fc061f95ee8fd6909052c9dde135
sambuchito <139599455+sambuchito@users.noreply.github.com>
2024-11-06 08:27:22 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from "./componentes/Card";
+
+function App() {
+   return (
+        <div className="App">
+      <h1>Carga de Estudiantes</h1>
+      <form>
+        
+      </form>
+          <Card />
+        </div>
+
+
+      );
+  
+    
+}
+
+export default App
+
